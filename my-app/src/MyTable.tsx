29bcd18295f517f4f1158654c3209f95a8eb5379
Karolis Blazevicius <blaz@danskebank.lt>
2018-06-03 21:05:34 +0300

    Can remove one order with button.

--- my-app/src/MyTable.tsx
+++ my-app/src/MyTable.tsx
@@ -2,26 +2,45 @@ import * as React from 'react';
 // import MyFirstRendererParams from './MyFirstRendererParams';
 import MyRowsComponent from './MyRowsComponent';
 import MyOrder from './MyOrder';
+import MyRowsComponentParams from './MyRowsComponentParams';
 
-class MyTable extends React.Component{
+class MyTable extends React.Component<{},MyRowsComponentParams>{
+
+  constructor(){
+    super({});
+
+    let orders:MyOrder[] = [
+      {Address:"Staneviciaus g", Phone: "23424"},
+      {Address:"Ateitieds g", Phone: "353535s"},
+      {Address:"Ateities g", Phone: "353535"}
+    ];
+
+    this.state = {orders:orders};
+  
+  }
+
+HandleClick = () => {
+  let currentOrders = this.state;
+  currentOrders.orders.pop();
+  this.setState(currentOrders);
+}
 
 public render(){ 
 
-  let orders:MyOrder[] = [
-    {Address:"Staneviciaus g", Phone: "23424"},
-    {Address:"Ateitieds g", Phone: "353535s"},
-    {Address:"Ateities g", Phone: "353535"}
-  ];
-
 return(
+  <div>
+    <button onClick={this.HandleClick}>
+      Add
+    </button>
   <table>    
     <tr>
       <td> ADDRESS </td>
       <td> PHONE </td>
       <td> NUMBER_OF_PIZZAS </td>
     </tr>
-    <MyRowsComponent orders={orders}/>
+    <MyRowsComponent orders={this.state.orders}/>
   </table>
+  </div>
   ); 
 }
 
