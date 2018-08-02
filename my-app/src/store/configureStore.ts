5705675a4f88c5ca1a8ad184c035da6c9bea1ef3
korulis <broliukas@gmail.com>
2018-08-02 23:06:30 +0300

    more types

--- my-app/src/store/configureStore.ts
+++ my-app/src/store/configureStore.ts
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+import GlobalState from './State';
 
-export default function configureStore(initialState?: Object) {
+export default function configureStore(initialState?: GlobalState) {
     return createStore(
         rootReducer,
         initialState,
